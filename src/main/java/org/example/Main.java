f4f4c7a9f0e930063461491fce26664ef0076bce
Sergey <sergeyraychenko@gmail.com>
2021-04-11 13:58:00 +0300

    final commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -11,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         List<Animal> animals = getAnimals();
 
-      //  The old way (Imperative)
+        //  The old way (Imperative)
 //        List<Animal> predators = new ArrayList<>();
 //        for (Animal animal: animals) {
 //            if (animal.getClassification().equals(Classification.PREDATOR)){
@@ -20,17 +17,18 @@ public class Main {
 //        }
 //        predators.forEach(System.out::println);
 
-       // New way (Declarative) Filter, Sort, All match, Any match, None match, None match
-      //   Max, Min, Group
+        // New way (Declarative) Filter, Sort, All match, Any match, None match, None match
+        //   Max, Min, Group
 
-         //Filter
+        //Filter
         List<Animal> predators = animals.stream()
                 .filter(animal -> animal.getClassification().equals(Classification.PREDATOR))
                 .collect(Collectors.toList());
 
         predators.forEach(System.out::println);
 
-         //Sort
+
+        //Sort
         List<Animal> sortedPredators = animals.stream()
                 .sorted(Comparator.comparing(Animal::getAge).reversed()
                         .thenComparing(Animal::getClassification)
@@ -39,11 +37,13 @@ public class Main {
 
         sortedPredators.forEach(System.out::println);
 
-       //  All match - Checks all entities in the collection are equal to a condition
+
+        //  All match - Checks all entities in the collection are equal to a condition
         boolean allMatch = animals.stream()
                 .allMatch(animal -> animal.getAge() > 10);
 
         System.out.println(allMatch); // false
+
 
         // Any match - Checks that at least one entity in the collection is equal to a condition
         boolean anyMatch = animals.stream()
@@ -51,23 +51,28 @@ public class Main {
 
         System.out.println(anyMatch); //true
 
-        // None match - If there is no entity equal to the condition, it displays true
 
+        // None match - If there is no entity equal to the condition, it displays true
         boolean noneMatch = animals.stream()
                 .noneMatch(animal -> animal.getName().equals("Слон"));
 
         System.out.println(noneMatch); //false
 
-         //Max - Returns optional
+
+        //Max - Returns optional
         animals.stream()
                 .max(Comparator.comparing(Animal::getAge))
                 .ifPresent(System.out::println);
+
+
         // Min - Returns optional
         animals.stream()
                 .min(Comparator.comparing(Animal::getAge))
                 .ifPresent(System.out::println);
+
+
         // Group
-        Map<Classification,List<Animal>> classificationListMap = animals.stream()
+        Map<Classification, List<Animal>> classificationListMap = animals.stream()
                 .collect(Collectors.groupingBy(Animal::getClassification));
 
         classificationListMap.forEach(((classification, animals1) -> {
@@ -77,6 +82,13 @@ public class Main {
         }));
 
 
+        //Find the name of the oldest predator
+        Optional<String> predatorName = animals.stream()
+                .filter(animal -> animal.getClassification().equals(Classification.PREDATOR))
+                .max(Comparator.comparing(Animal::getAge))
+                .map(Animal::getName);
+
+        System.out.println(predatorName);
     }
 
     private static List<Animal> getAnimals() {
